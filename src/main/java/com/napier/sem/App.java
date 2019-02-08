c0bfcbf9d7c3c18afc188f3124cbd957d3cb83b1
emilytaylor97 <40310983@live.napier.ac.uk>
2019-02-08 14:09:52 +0000

    test commit

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -27,5 +27,7 @@ public class App
         // Check document in collection
         Document myDoc = collection.find().first();
         System.out.println(myDoc.toJson());
+
+        //delete this
     }
 }
