e36be0a4b4c59e417324f583780b7856f51f06f2
amelia-handley <amelia.handley7@gmail.com>
2019-02-15 14:04:32 +0000

    Starting Countries.app

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -12,7 +12,7 @@ public class App
         // Connect to database
         a.connect();
         // Get Employee
-        Employee emp = a.getEmployee(255530);
+        Countries emp = a.getEmployee(255530);
         // Display results
         a.displayEmployee(emp);
 
---
+++ src/main/java/com/napier/sem/Countries.java
@@ -0,0 +1,44 @@
+package com.napier.sem;
+
+/**
+ * Represents an country
+ */
+
+// code, name, continent, region, population, capital
+
+public class Countries{
+    /**
+     * Employee country code
+     */
+    public int country_code;
+
+    /**
+     * Employee's country name
+     */
+    public String country_name;
+
+    /**
+     * Employee's continent
+     */
+    public String continent;
+
+    /**
+     * Employee's population
+     */
+    public int population;
+
+    /**
+     * Employee's capital
+     */
+    public String capital;
+
+    /**
+     * Employee's region
+     */
+    public String region;
+
+    }
+
+
+
+
